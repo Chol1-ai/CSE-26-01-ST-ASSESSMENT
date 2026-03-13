b85e5b9985f0d6de133f953bb443a591fae76d8c
Hana <momy.me.us@gmail.com>
2026-03-13 13:23:46 +0300

    working on Sccessful message style

--- HanaDebayAlemayehu/server.js
+++ HanaDebayAlemayehu/server.js
@@ -22,4 +22,4 @@ app.use('/api/users', userRoutes);
 
 
 
-app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
+app.listen(process.env.PORT, () => console.log(`Server running http://localhost:${process.env.PORT}`));
